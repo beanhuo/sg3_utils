f1200a47413966aac494fd1f9374fd0e018148fe
beanhuo <beanhuo@micron.com>
2017-12-18 18:19:46 +0100

    Append compile flags '-fPIE -pie'
    This is used to avoid following problem when
    you run the ELF file on your object platform.
    
     "Android 5.0 'error: only position independent
     executables (PIE) are support"

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./configure CFLAGS=-static --prefix=$PWD/out/ --host=aarch64-linux-android LD=aarch64-linux-android-ld CC=aarch64-linux-android-gcc
+./configure CFLAGS='-static -fPIE -pie' --prefix=$PWD/out/ --host=aarch64-linux-android LD=aarch64-linux-android-ld CC=aarch64-linux-android-gcc
 # following steps run mannaully
 #make
 #make install
